e6414a701643bba4050729d6218204a62ff00d24
mjedwards <jairedwards20@gmail.com>
2021-12-07 05:24:55 -0500

    fixed minor issue

--- src/components/forms/contact.js
+++ src/components/forms/contact.js
@@ -73,7 +73,7 @@ export default function Contact() {
         </div>
         <button
           class="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-          type="button"
+          type="submit"
         >
           Submit
         </button>
