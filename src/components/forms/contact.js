8cb33fb06f5f6949232ca02ecaaf860036dce213
mjedwards <jairedwards20@gmail.com>
2021-12-07 05:37:19 -0500

    fixed minor issue

--- src/components/forms/contact.js
+++ src/components/forms/contact.js
@@ -73,7 +73,7 @@ export default function Contact() {
         </div>
         <div data-netlify-recaptcha="true"></div>
         <button
-          class="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+          className="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
           type="submit"
         >
           Submit
