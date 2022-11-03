1ff76b0a64bddd46e837b846332c7803fb5c4760
mjedwards <jairedwards20@gmail.com>
2022-11-03 11:25:10 -0400

    deploy test

--- src/components/about/atoms/Hero.js
+++ src/components/about/atoms/Hero.js
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
     <div className="bg-gray-50">
       <h1 className="text-blue-900 pt-20 pb-20 text-5xl lg:py-28 lg:text-6xl font-bold">
-        Our Vision and Mission
+        Our Vision and Mission 
       </h1>
     </div>
   );
