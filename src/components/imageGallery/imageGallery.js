0009bc920c3d014377d76e514f455a028e58cd28
mjedwards <jairedwards20@gmail.com>
2024-04-21 18:15:13 -0400

    added new photos

--- src/components/imageGallery/imageGallery.js
+++ src/components/imageGallery/imageGallery.js
@@ -2,28 +2,28 @@ import React from "react";
 import Images from "./images";
 
 export default function ImageGallery() {
-  return (
-    <div className="w-screen">
-      <div className="space-y-2 lg:space-y-0 lg:grid lg:grid-cols-3 w-screen">
-        <div className="w-full rounded">
-          <img src={Images[7]} alt="cool watchdog" />
-        </div>
-        <div className="w-full rounded">
-          <img src={Images[4]} alt="cool watchdog" />
-        </div>
-        <div className="w-full rounded">
-          <img src={Images[21]} alt="cool watchdog" />
-        </div>
-        <div className="w-full rounded">
-          <img src={Images[22]} alt="cool watchdog" />
-        </div>
-        <div className="w-full rounded">
-          <img src={Images[2]} alt="cool watchdog" />
-        </div>
-        <div className="w-full rounded">
-          <img src={Images[13]} alt="cool watchdog" />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className='w-screen'>
+			<div className='space-y-2 lg:space-y-0 lg:grid lg:grid-cols-3 w-screen'>
+				<div className='w-full rounded'>
+					<img src={Images[45]} alt='mentor breakfast' />
+				</div>
+				<div className='w-full rounded'>
+					<img src={Images[47]} alt='mentor breakfast' />
+				</div>
+				<div className='w-full rounded'>
+					<img src={Images[48]} alt='mentor breakfast' />
+				</div>
+				<div className='w-full rounded'>
+					<img src={Images[2]} alt='mentor breakfast' />
+				</div>
+				<div className='w-full rounded'>
+					<img src={Images[53]} alt='mentor breakfast' />
+				</div>
+				<div className='w-full rounded'>
+					<img src={Images[13]} alt='mentor breakfast' />
+				</div>
+			</div>
+		</div>
+	);
 }
